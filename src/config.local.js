e32353b6889c63e5ada14b9cb917285924e711cd
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-03-11 12:37:57 +0200

    (api): Add api creds (restricted to a single domain)

--- src/config.local.js
+++ src/config.local.js
@@ -1,5 +1,5 @@
-var YOUR_CLIENT_ID = 'your id here';
+var YOUR_CLIENT_ID = '704903675016-a979h5ibo69ivctsj28hvr3oqeovvggu.apps.googleusercontent.com';
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 var SCOPE = 'https://www.googleapis.com/auth/drive.readonly';
-var YOUR_API_KEY = 'your key here';
+var YOUR_API_KEY = 'AIzaSyDWFyhkmsccAznUUDpeQAIIc5y1Muy2c8E';
 
